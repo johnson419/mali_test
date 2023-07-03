025fcc23d6af4e8b16ff87b4fd23a57edaa02eb5
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-07-04 01:51:09 +0300

    fixed switch error

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import CRUDForm from './components/CRUDForm';
-
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register/>
-          </Route>
-          <Route path="/crud">
-            <CRUDForm />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/crud" element={<CRUDForm />} />
+        </Routes>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+
 
