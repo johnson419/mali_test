ef741f03a8c6039d647358e749a0e9a9ea07c421
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-07-04 03:14:54 +0300

    initiial configurations for springboot

---
+++ src/backend/mali/src/main/java/com/example/mali/MaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.mali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaliApplication.class, args);
+	}
+
+}
---
+++ src/backend/mali/src/test/java/com/example/mali/MaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
